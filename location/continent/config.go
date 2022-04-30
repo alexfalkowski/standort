c1d333e6ff391e5c5ad981bd4fe05e21a8188e19
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-04-30 07:33:49 +0200

    refactor(continent): default file locations

--- location/continent/config.go
+++ location/continent/config.go
@@ -1,5 +1,9 @@
 package continent
 
+import (
+	"fmt"
+)
+
 // Config for continent.
 type Config struct {
 	AfricaPath       string `yaml:"africa_path"`
@@ -9,4 +13,64 @@ type Config struct {
 	AsiaPath         string `yaml:"asia_path"`
 	EuropePath       string `yaml:"europe_path"`
 	SouthAmericaPath string `yaml:"south_america_path"`
+}
+
+func (c *Config) GetAfricaPath() string {
+	if c.AfricaPath != "" {
+		return c.AfricaPath
+	}
+
+	return c.path("africa")
+}
+
+func (c *Config) GetNorthAmericaPath() string {
+	if c.NorthAmericaPath != "" {
+		return c.NorthAmericaPath
+	}
+
+	return c.path("north_america")
+}
+
+func (c *Config) GetOceaniaPath() string {
+	if c.OceaniaPath != "" {
+		return c.OceaniaPath
+	}
+
+	return c.path("oceania")
+}
+
+func (c *Config) GetAntarcticaPath() string {
+	if c.AntarcticaPath != "" {
+		return c.AntarcticaPath
+	}
+
+	return c.path("antarctica")
+}
+
+func (c *Config) GetAsiaPath() string {
+	if c.AsiaPath != "" {
+		return c.AsiaPath
+	}
+
+	return c.path("asia")
+}
+
+func (c *Config) GetEuropePath() string {
+	if c.EuropePath != "" {
+		return c.EuropePath
+	}
+
+	return c.path("europe")
+}
+
+func (c *Config) GetSouthAmericaPath() string {
+	if c.SouthAmericaPath != "" {
+		return c.SouthAmericaPath
+	}
+
+	return c.path("south_america")
+}
+
+func (c *Config) path(name string) string {
+	return fmt.Sprintf("assets/%s.geojson", name)
 }
