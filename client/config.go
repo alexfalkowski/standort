a6b3bb892e7da4de4d1442553580729a393fbba6
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-15 05:33:32 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.139.1

--- client/config.go
+++ client/config.go
@@ -7,6 +7,6 @@ import (
 
 // Config for client.
 type Config struct {
-	V1 v1.Config `yaml:"v1" json:"v1" toml:"v1"`
-	V2 v2.Config `yaml:"v2" json:"v2" toml:"v2"`
+	V1 v1.Config `yaml:"v1,omitempty" json:"v1,omitempty" toml:"v1,omitempty"`
+	V2 v2.Config `yaml:"v2,omitempty" json:"v2,omitempty" toml:"v2,omitempty"`
 }
