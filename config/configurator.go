6728dc9a6c7784f54924743ce857503f82d37f82
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-24 13:22:56 +0100

    fix(config): check presense (#606)

--- config/configurator.go
+++ config/configurator.go
@@ -18,19 +18,39 @@ func NewConfigurator(i *cmd.InputConfig) (config.Configurator, error) {
 }
 
 func ipConfig(cfg config.Configurator) *ip.Config {
-	return cfg.(*Config).Location.IP
+	c := cfg.(*Config)
+	if c.Location == nil {
+		return nil
+	}
+
+	return c.Location.IP
 }
 
 func continentConfig(cfg config.Configurator) *continent.Config {
-	return cfg.(*Config).Location.Continent
+	c := cfg.(*Config)
+	if c.Location == nil {
+		return nil
+	}
+
+	return c.Location.Continent
 }
 
 func v1Client(cfg config.Configurator) *v1.Config {
-	return cfg.(*Config).Client.V1
+	c := cfg.(*Config)
+	if c.Client == nil {
+		return nil
+	}
+
+	return c.Client.V1
 }
 
 func v2Client(cfg config.Configurator) *v2.Config {
-	return cfg.(*Config).Client.V2
+	c := cfg.(*Config)
+	if c.Client == nil {
+		return nil
+	}
+
+	return c.Client.V2
 }
 
 func healthConfig(cfg config.Configurator) *health.Config {
