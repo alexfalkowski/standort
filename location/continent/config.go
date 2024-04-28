0c9b59556b7998a83735183405e7afd78705c6f3
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-28 19:38:40 +0200

    fix(continent): specify paths

--- location/continent/config.go
+++ location/continent/config.go
@@ -1,9 +1,5 @@
 package continent
 
-import (
-	"fmt"
-)
-
 // IsEnabled for continent.
 func IsEnabled(c *Config) bool {
 	return c != nil
@@ -17,62 +13,4 @@ type Config struct {
 	AsiaPath         string `yaml:"asia_path,omitempty" json:"asia_path,omitempty" toml:"asia_path,omitempty"`
 	EuropePath       string `yaml:"europe_path,omitempty" json:"europe_path,omitempty" toml:"europe_path,omitempty"`
 	SouthAmericaPath string `yaml:"south_america_path,omitempty" json:"south_america_path,omitempty" toml:"south_america_path,omitempty"`
-}
-
-// GetAfricaPath of geojson.
-func (c *Config) GetAfricaPath() string {
-	if c.AfricaPath != "" {
-		return c.AfricaPath
-	}
-
-	return c.path("africa")
-}
-
-// GetNorthAmericaPath of geojson.
-func (c *Config) GetNorthAmericaPath() string {
-	if c.NorthAmericaPath != "" {
-		return c.NorthAmericaPath
-	}
-
-	return c.path("north_america")
-}
-
-// GetOceaniaPath of geojson.
-func (c *Config) GetOceaniaPath() string {
-	if c.OceaniaPath != "" {
-		return c.OceaniaPath
-	}
-
-	return c.path("oceania")
-}
-
-// GetAsiaPath of geojson.
-func (c *Config) GetAsiaPath() string {
-	if c.AsiaPath != "" {
-		return c.AsiaPath
-	}
-
-	return c.path("asia")
-}
-
-// GetEuropePath of geojson.
-func (c *Config) GetEuropePath() string {
-	if c.EuropePath != "" {
-		return c.EuropePath
-	}
-
-	return c.path("europe")
-}
-
-// GetSouthAmericaPath of geojson.
-func (c *Config) GetSouthAmericaPath() string {
-	if c.SouthAmericaPath != "" {
-		return c.SouthAmericaPath
-	}
-
-	return c.path("south_america")
-}
-
-func (c *Config) path(name string) string {
-	return fmt.Sprintf("/assets/%s.geojson", name)
 }
