2200c23f79975804d787d7531234e838b9525df7
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-05 06:47:03 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.178.0 (#713)

--- client/config.go
+++ client/config.go
@@ -5,6 +5,11 @@ import (
 	v2 "github.com/alexfalkowski/standort/client/v2/config"
 )
 
+// IsEnabled for client.
+func IsEnabled(cfg *Config) bool {
+	return cfg != nil
+}
+
 // Config for client.
 type Config struct {
 	V1 *v1.Config `yaml:"v1,omitempty" json:"v1,omitempty" toml:"v1,omitempty"`
