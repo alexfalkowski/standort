30ead76f46445386781229e86b1e4388e7874994
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-21 20:42:30 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.198.0

--- client/module.go
+++ client/module.go
@@ -2,10 +2,12 @@ package client
 
 import (
 	v1 "github.com/alexfalkowski/standort/client/v1"
+	v2 "github.com/alexfalkowski/standort/client/v2"
 	"go.uber.org/fx"
 )
 
-// ClientModule for fx.
-var ClientModule = fx.Options(
+// Module for fx.
+var Module = fx.Options(
 	v1.Module,
+	v2.Module,
 )
