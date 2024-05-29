de531978194a9b012e7fc6a449c1ff7b4766ed21
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-29 19:20:18 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.207.0

--- cmd/module.go
+++ cmd/module.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	"github.com/alexfalkowski/go-service/cmd"
+	"github.com/alexfalkowski/go-service/env"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
 	cmd.Module,
+	env.Module,
 	fx.Provide(NewVersion),
 )
