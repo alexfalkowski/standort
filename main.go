b750cf35f0081ba8984ef3b68d6532f1ec44ccb6
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-30 08:52:37 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.209.0 (#764)

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 func command() *sc.Command {
 	c := sc.New(cmd.Version)
-	c.RegisterInput("env:STANDORT_CONFIG_FILE")
+	c.RegisterInput(c.Root(), "env:STANDORT_CONFIG_FILE")
 	c.AddServer(cmd.ServerOptions...)
 
 	return c
