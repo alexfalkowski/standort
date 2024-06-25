f382db3d5a6d9104eecebfe3c462d582b1f1a339
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-06-25 08:12:39 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.221.0 (#794)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -1,21 +1,11 @@
 package http
 
 import (
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/net/http"
 	"github.com/alexfalkowski/standort/location"
-	"go.uber.org/fx"
 )
 
 type (
-	// RegisterParams for HTTP.
-	RegisterParams struct {
-		fx.In
-
-		Marshaller *marshaller.Map
-		Mux        http.ServeMux
-		Location   *location.Location
-	}
 
 	// Server for HTTP.
 	Server struct {
@@ -29,18 +19,9 @@ type (
 )
 
 // Register for HTTP.
-func Register(params RegisterParams) error {
-	s := &Server{location: params.Location}
+func Register(location *location.Location) {
+	s := &Server{location: location}
 
-	ih := http.NewHandler[GetLocationByIPRequest](params.Mux, params.Marshaller, &ipErrorer{})
-	if err := ih.Handle("POST", "/v1/ip", s.GetLocationByIP); err != nil {
-		return err
-	}
-
-	ch := http.NewHandler[GetLocationByLatLngRequest](params.Mux, params.Marshaller, &coordinateErrorer{})
-	if err := ch.Handle("POST", "/v1/coordinate", s.GetLocationByLatLng); err != nil {
-		return err
-	}
-
-	return nil
+	http.Handler("POST /v1/ip", &ipErrorer{}, s.GetLocationByIP)
+	http.Handler("POST /v1/coordinate", &coordinateErrorer{}, s.GetLocationByLatLng)
 }
