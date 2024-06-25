73a42b8a5e95c3ae119e614d298bf84e7ee10eb6
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-25 08:03:07 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.221.0

--- server/v2/transport/http/http.go
+++ server/v2/transport/http/http.go
@@ -1,22 +1,11 @@
 package http
 
 import (
-	"github.com/alexfalkowski/go-service/marshaller"
 	"github.com/alexfalkowski/go-service/net/http"
 	"github.com/alexfalkowski/standort/location"
-	"go.uber.org/fx"
 )
 
 type (
-	// RegisterParams for HTTP.
-	RegisterParams struct {
-		fx.In
-
-		Marshaller *marshaller.Map
-		Mux        http.ServeMux
-		Location   *location.Location
-	}
-
 	// Server for HTTP.
 	Server struct {
 		location *location.Location
@@ -29,13 +18,8 @@ type (
 )
 
 // Register for HTTP.
-func Register(params RegisterParams) error {
-	s := &Server{location: params.Location}
+func Register(location *location.Location) {
+	s := &Server{location: location}
 
-	lh := http.NewHandler[GetLocationRequest](params.Mux, params.Marshaller, &locationErrorer{})
-	if err := lh.Handle("POST", "/v2/location", s.GetLocation); err != nil {
-		return err
-	}
-
-	return nil
+	http.Handler("POST /v2/location", &locationErrorer{}, s.GetLocation)
 }
