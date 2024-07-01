14f500c0c975e5aa75ca63eae21c34fe52b1c9c5
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-01 21:54:57 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.230.0

--- server/v2/transport/http/http.go
+++ server/v2/transport/http/http.go
@@ -5,11 +5,6 @@ import (
 	"github.com/alexfalkowski/standort/server/service"
 )
 
-// Error for HTTP.
-type Error struct {
-	Message string `json:"message,omitempty"`
-}
-
 // Register for HTTP.
 func Register(service *service.Service) {
 	http.Handle("/v2/location", &locationHandler{service: service})
