ee3e6a358948447bf247e028e0d00063b83eeb1b
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-05 12:09:30 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.238.0 (#829)

--- server/v1/transport/http/http.go
+++ server/v1/transport/http/http.go
@@ -3,19 +3,19 @@ package http
 import (
 	"net/http"
 
-	nh "github.com/alexfalkowski/go-service/net/http"
+	"github.com/alexfalkowski/go-service/net/http/rpc"
 	"github.com/alexfalkowski/standort/location"
 )
 
 // Register for HTTP.
 func Register(location *location.Location) {
-	nh.Handle("/v1/ip", &ipHandler{location: location})
-	nh.Handle("/v1/coordinate", &coordinateHandler{location: location})
+	rpc.Handle("/v1/ip", &ipHandler{location: location})
+	rpc.Handle("/v1/coordinate", &coordinateHandler{location: location})
 }
 
 func handleError(err error) error {
 	if location.IsNotFound(err) {
-		return nh.Error(http.StatusNotFound, err.Error())
+		return rpc.Error(http.StatusNotFound, err.Error())
 	}
 
 	return err
--- server/v2/transport/http/http.go
+++ server/v2/transport/http/http.go
@@ -3,18 +3,18 @@ package http
 import (
 	"net/http"
 
-	nh "github.com/alexfalkowski/go-service/net/http"
+	"github.com/alexfalkowski/go-service/net/http/rpc"
 	"github.com/alexfalkowski/standort/server/location"
 )
 
 // Register for HTTP.
 func Register(service *location.Locator) {
-	nh.Handle("/v2/location", &locationHandler{service: service})
+	rpc.Handle("/v2/location", &locationHandler{service: service})
 }
 
 func handleError(err error) error {
 	if location.IsNotFound(err) {
-		return nh.Error(http.StatusNotFound, err.Error())
+		return rpc.Error(http.StatusNotFound, err.Error())
 	}
 
 	return err
