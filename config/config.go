7ba39dd5a312ecbe66d027b07e77685a52c022fa
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-18 20:57:01 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.343.0
    https://github.com/alexfalkowski/go-service/pull/1085

--- config/config.go
+++ config/config.go
@@ -11,15 +11,6 @@ type Config struct {
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
 }
 
-// Valid or error.
-func (c Config) Valid() error {
-	if c.Config == nil {
-		return config.ErrInvalidConfig
-	}
-
-	return c.Config.Valid()
-}
-
 func decorateConfig(cfg *Config) *config.Config {
 	return cfg.Config
 }
