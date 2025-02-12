227af1461f69bd210f68f18426b764f1bf4cc861
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-12 18:11:04 +0100

    feat(cmd): move to use register (#1151)

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ func main() {
 }
 
 func command() *sc.Command {
-	c := sc.New(cmd.Version)
-	c.RegisterInput(c.Root(), "env:STANDORT_CONFIG_FILE")
-	c.AddServer("server", "Start standort server", cmd.ServerOptions...)
+	command := sc.New(cmd.Version)
+	command.RegisterInput(command.Root(), "env:STANDORT_CONFIG_FILE")
 
-	return c
+	cmd.RegisterServer(command)
+
+	return command
 }
