63ce3e822807b9a598f9cec555c14982c0d01751
Alex Falkowski <alexrfalkowski@gmail.com>
2025-04-11 20:35:53 +0200

    feat(api): use grpc full method name

--- test/lib/standort/v1/http.rb
+++ test/lib/standort/v1/http.rb
@@ -4,11 +4,11 @@ module Standort
   module V1
     class HTTP < Nonnative::HTTPClient
       def get_location_by_ip(ip, opts = {})
-        post('/v1/ip', { ip: URI::Parser.new.escape(ip) }.to_json, opts)
+        post('/standort.v1.Service/GetLocationByIP', { ip: URI::Parser.new.escape(ip) }.to_json, opts)
       end
 
       def get_location_by_lat_lng(lat, lng, opts = {})
-        post('/v1/coordinate', { lat: lat.to_f, lng: lng.to_f }.to_json, opts)
+        post('/standort.v1.Service/GetLocationByLatLng', { lat: lat.to_f, lng: lng.to_f }.to_json, opts)
       end
     end
   end
