6855f7d4564bada390ec9b9425f803154826fec8
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-06-09 08:48:26 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.60.0 (#1436)
    https://github.com/alexfalkowski/go-service/pull/1707

---
+++ internal/health/observer.go
@@ -0,0 +1,23 @@
+package health
+
+import (
+	health "github.com/alexfalkowski/go-health/server"
+	grpc "github.com/alexfalkowski/go-service/v2/transport/grpc/health"
+	http "github.com/alexfalkowski/go-service/v2/transport/http/health"
+)
+
+func httpHealthObserver(server *health.Server) *http.HealthObserver {
+	return &http.HealthObserver{Observer: server.Observe("online")}
+}
+
+func httpLivenessObserver(server *health.Server) *http.LivenessObserver {
+	return &http.LivenessObserver{Observer: server.Observe("noop")}
+}
+
+func httpReadinessObserver(server *health.Server) *http.ReadinessObserver {
+	return &http.ReadinessObserver{Observer: server.Observe("noop")}
+}
+
+func grpcObserver(server *health.Server) *grpc.Observer {
+	return &grpc.Observer{Observer: server.Observe("noop")}
+}
--- internal/health/registrations.go
+++ internal/health/registrations.go
@@ -5,19 +5,10 @@ import (
 	"github.com/alexfalkowski/go-health/server"
 	"github.com/alexfalkowski/go-service/v2/health"
 	"github.com/alexfalkowski/go-service/v2/time"
-	"go.uber.org/fx"
 )
 
-// Params for health.
-type Params struct {
-	fx.In
-
-	Health *Config
-}
-
-// NewRegistrations for health.
-func NewRegistrations(params Params) health.Registrations {
-	d := time.MustParseDuration(params.Health.Duration)
+func registrations(cfg *Config) health.Registrations {
+	d := time.MustParseDuration(cfg.Duration)
 	registrations := health.Registrations{
 		server.NewRegistration("noop", d, checker.NewNoopChecker()),
 		server.NewOnlineRegistration(d, d),
