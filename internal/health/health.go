289b01a3d9b5e82d2e1e5ac7ee17bc6c2dad39d1
Alex Falkowski <alexrfalkowski@gmail.com>
2025-07-26 18:26:55 +0200

    feat(deps): update github.com/alexfalkowski/go-health/v2 to v2.3.0

---
+++ internal/health/health.go
@@ -0,0 +1,43 @@
+package health
+
+import (
+	"github.com/alexfalkowski/go-health/v2/checker"
+	"github.com/alexfalkowski/go-health/v2/server"
+	"github.com/alexfalkowski/go-service/v2/env"
+	"github.com/alexfalkowski/go-service/v2/errors"
+	"github.com/alexfalkowski/go-service/v2/health"
+	"github.com/alexfalkowski/go-service/v2/time"
+	v1 "github.com/alexfalkowski/standort/v2/api/standort/v1"
+	v2 "github.com/alexfalkowski/standort/v2/api/standort/v2"
+)
+
+func register(name env.Name, srv *server.Server, cfg *Config) {
+	d := time.MustParseDuration(cfg.Duration)
+	regs := health.Registrations{
+		server.NewRegistration("noop", d, checker.NewNoopChecker()),
+		server.NewOnlineRegistration(d, d),
+	}
+
+	srv.Register(name.String(), regs...)
+	srv.Register(v1.Service_ServiceDesc.ServiceName, regs...)
+	srv.Register(v2.Service_ServiceDesc.ServiceName, regs...)
+}
+
+func httpHealthObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "healthz", "online")
+}
+
+func httpLivenessObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "livez", "noop")
+}
+
+func httpReadinessObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "readyz", "noop")
+}
+
+func grpcObserver(server *server.Server) error {
+	return errors.Join(
+		server.Observe(v1.Service_ServiceDesc.ServiceName, "grpc", "noop"),
+		server.Observe(v2.Service_ServiceDesc.ServiceName, "grpc", "noop"),
+	)
+}
--- test/features/step_definitions/health/transport/grpc/observability_steps.rb
+++ test/features/step_definitions/health/transport/grpc/observability_steps.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 When('the system requests the health status with gRPC') do
-  request = Grpc::Health::V1::HealthCheckRequest.new
+  request = Grpc::Health::V1::HealthCheckRequest.new(service: 'standort.v2.Service')
   @response = Standort.health_grpc.check(request)
 end
 
